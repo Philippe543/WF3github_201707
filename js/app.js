7f21a354212e12b11a625f4457b68b145e56874c
Philippe543 <peronzone@yahoo.fr>
2017-07-19 15:34:11 +0200

    ajax

---
+++ js/app.js
@@ -0,0 +1,23 @@
+    $(document).ready(function(){
+
+        $.ajax({
+            type: 'GET',
+            datatype: 'json',
+            url: 'data/data.json',
+
+            success:function(data){
+                console.log(data);
+
+                for( var i = 0 ; i <data.length;i++){
+                    $('nav ul').append('<li><a href="' +data[i].anchor +'">' + data[i].content +'</a></li>');
+                }
+
+            },
+            error:function(err){
+                console.log(err);
+            },
+            complete:function(){
+                console.log('fin de la requête');
+            }
+        })
+    })
